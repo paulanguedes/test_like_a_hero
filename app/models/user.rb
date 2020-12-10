5d5aab6c05a45ce87b5f60e8c8a0a865a1bac78b
paulanguedes <paulanguedes@gmail.com>
2020-12-10 17:39:10 -0300

    preparing enviroment for tests and creating user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    # opções dentro do campo kind
+  enum kind: %i[knight wizard]
+    #validação do próprio Rails
+  validates :level, :numericality { greater_than: 0, less_than_or_equal_to: 99 }
+
+  # método customizado
+  def title
+    "#{kind} #{nickname} ##{level}"
+  end
+end
