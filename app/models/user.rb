e30c207bc984868791cc321dcd6c00f840c0b3d0
paulanguedes <paulanguedes@gmail.com>
2020-12-10 18:19:15 -0300

    user model's basic tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    # opções dentro do campo kind
+  # opções dentro do campo kind
   enum kind: %i[knight wizard]
-    #validação do próprio Rails
-  validates :level, :numericality { greater_than: 0, less_than_or_equal_to: 99 }
+  # validação do próprio Rails
+  validates :level, numericality: { greater_than: 0, less_than_or_equal_to: 99 }
 
   # método customizado
   def title
